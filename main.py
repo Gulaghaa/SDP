b8795aa7c45f90f95cfb84d13e0f1f928ffe5b61
Javid Mammadzada <mammadzadacavid0@gmail.com>
2025-02-27 10:46:34 +0400

    Update CORS

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ from ultralytics import YOLO
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
