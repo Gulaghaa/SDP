05aaf2524975b049af8e9504d55159baa3b24afd
Javid Mammadzada <mammadzadacavid0@gmail.com>
2025-02-27 16:50:01 +0400

    Update CORS

--- main.py
+++ main.py
@@ -8,16 +8,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins, 
-    allow_credentials=True,
-    allow_methods=["*"],
+    allow_origins=["*"], 
     allow_headers=["*"],
 )
 
